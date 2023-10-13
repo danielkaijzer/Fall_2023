a2b0909f97b09204bb1a540cc2144be73843e7e2
danielkaijzer <danielkaijzer00@gmail.com>
2023-10-13 00:11:48 -0400

    First solution done, feeling good

--- LC929_Unique_Email.cpp
+++ LC929_Unique_Email.cpp
@@ -1,3 +1,14 @@
+/**
+ * @file LC929_Unique_Email.cpp
+ * @author Daniel Kaijzer
+ * @brief 
+ * @version Solution v1
+ * @date 2023-10-13
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
 #include <iostream>
 #include <vector>
 #include <unordered_set>
@@ -6,22 +17,52 @@ int numUniqueEmails(std::vector<std::string>& emails)
 {
     int count=0;
 
+    std::unordered_set<std::string> hashset;
+
     for (auto itr = emails.begin(); itr != emails.end(); itr++)
     {
-        std::cout << *itr << std::endl;
+        std::string current = "";
+
+        bool ignore = false;
+        bool domain = false;
+
+        // remove variations to make email verifiably unique
+        for (auto c : *itr){
+            if (c != '.' && domain == false){ // remove dots
+                if (c != '+' && c!= '@' && ignore == false){
+                    current += c;
+                }
+                else if (c == '+'){
+                    ignore = true;
+                }
+                else if (c == '@'){
+                    ignore = false;
+                    domain = true;
+                    current += c;
+                }
+            }
+            else if (domain == true){
+                current += c;
+            }
+        }
+
+        if (hashset.count(current) == 0){
+            hashset.insert(current);
+        }
+
+        std::cout << current << std::endl;
     }
-    // remove variations
 
-    // add email before @
-
-    return 0;
+    return hashset.size();
     
 }
 
 int main(){
-    std::vector<std::string> emails1{"test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com"};
 
-    std::cout << numUniqueEmails(emails1) << std::endl;
+    
+    std::vector<std::string> emails1{"test.email+alex@leetcode.com", "test.email@leetcode.com"};
+
+    std::cout << numUniqueEmails(emails1) << std::endl; // answer should be 1
 
     return 0;
 }
