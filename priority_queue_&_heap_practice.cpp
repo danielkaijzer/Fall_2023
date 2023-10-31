41498921d169c5ecffc3acbc3cdae304e8453a84
danielkaijzer <danielkaijzer00@gmail.com>
2023-10-30 21:50:02 -0400

    Started Andrej Karpathy's Makemore lecture. So far we've loaded in the text file that we will train our language model on. Then we began setting up a bigram language model

--- priority_queue_&_heap_practice.cpp
+++ priority_queue_&_heap_practice.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <stdexcept>
 
 
 class BinaryHeap{
@@ -7,11 +8,17 @@ class BinaryHeap{
 public:
 
 BinaryHeap( int capacity = 100 );
-BinaryHeap( const std::vector<int> & items );
+BinaryHeap( const std::vector<int> & items ){
+    for (int i = 0; i < items.size(); ++i){
+        array[i+1] = items[i];
+    }
+    buildHeap();
+}
 
 bool isEmpty( ) const{
     return (currentSize == 0);
 }
+
 // const int & findMin( ) const;
 
 void insert( const int & x ){
@@ -35,19 +42,47 @@ void insert( const int & x ){
 // void insert( int && x );
 void deleteMin( ){
     if (isEmpty()){
-        return;
+        throw std::underflow_error("List is empty");
     }
     array[1] = std::move(array[currentSize--]);
     percolateDown(1);
 }
 
 void deleteMin( int & minItem ){
+    if (isEmpty()){
+        throw std::underflow_error("List is empty");
+    }
 
+    minItem = std::move(array[1]); // new minItem is taken from root
+    array[1] = std::move(array[currentSize--]); // move last element to root
+    percolateDown(1); // create hole at root
 }
 
-void percolateDown( int hole );
+// in percolateDown, the hole moves down from root 
+// until it reaches correct location
+void percolateDown( int hole ){
+    int child;
+    int tmp = std::move(array[hole]);
 
-void buildHeap( );
+    for (; hole * 2 <= currentSize; hole = child){
+        child = hole * 2;
+        if (child != currentSize && array[child+1] < array[child]){
+            ++child;
+        }
+        if (array[child] < tmp){
+            array[hole] = std::move(array[child]);
+        }
+        else
+            break;
+    }
+    array[hole] = std::move(tmp);
+}
+
+void buildHeap( ){
+    for (int i = currentSize/2; i > 0; --i){
+        percolateDown(i);
+    }
+}
 
 // void makeEmpty( ); // repeatedly call deleteMin until size == 0
 
