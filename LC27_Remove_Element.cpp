919bf2dcbc7031480d78860c9b96451312a766b9
danielkaijzer <danielkaijzer00@gmail.com>
2024-01-10 21:48:42 -0500

    Practicing

---
+++ LC27_Remove_Element.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+// everytime you find an element equal to val, you swap
+// so that all deleted elements go to back of array
+
+using namespace std;
+
+int removeElement(vector<int>& nums, int val) {
+
+    int k = 0;
+
+
+    for (int i = 0; i < nums.size(); ++i){
+        
+    }
+}
+
+int main(){
+
+}
